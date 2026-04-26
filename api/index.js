f628cddf325f2618f02b78322f3bbd31d3161473
Ahmad030687 <ahmadalisafdar86@gmail.com>
2026-04-26 22:07:11 +0500

    Delete api directory

--- api/index.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    res.status(200).json({
-        bot: 'RDX Bot',
-        status: 'Active',
-        owner: 'Ahmad RDX',
-        message: 'Vercel Working Perfectly!'
-    });
-}
